23ef20f4ed17c48ae54ae65f6a8ed8cebabe9715
jonas-kell <jonas-kell@web.de>
2021-08-27 21:18:32 +0200

    78%

--- logic/perceptron.py
+++ logic/perceptron.py
@@ -11,7 +11,7 @@ import logic.perceptron_ttt_interface as interface
 inputs = 9
 outputs = 9
 hidden = 30
-learn_rate = 0.1
+learn_rate = 0.001
 
 
 def random_initial_matrices(filename):
@@ -152,6 +152,6 @@ def learn_logic_test():
 
 if __name__ == "__main__":
     train_perceptron(
-        "tic-tac-toe", 10000, interface.input_output_pair, init_random=False
+        "tic-tac-toe", 20000, interface.input_output_pair, init_random=False
     )
     # learn_logic_test()
