27f53dbfe7e891731087f442517447eedc697eaa
jonas-kell <jonas-kell@web.de>
2021-09-05 02:34:38 +0200

    4000000 with bias

--- logic/perceptron.py
+++ logic/perceptron.py
@@ -9,6 +9,7 @@ import logic.perceptron_ttt_interface as interface
 
 # cache function for many-times-execution
 stored_weights1 = None
+stored_bias_1 = None
 stored_weights2 = None
 
 # control the properties of the neural net
@@ -20,46 +21,51 @@ learn_rate = 0.001
 
 def random_initial_matrices(filename):
     store_matrix_to_file(np.random.rand(hidden, inputs) * 2 - 1, filename + "1")
+    store_matrix_to_file(np.random.rand(hidden, 1) * 2 - 1, filename + "1b")
     store_matrix_to_file(np.random.rand(outputs, hidden) * 2 - 1, filename + "2")
 
 
 def get_output_simple(input, filename="tic-tac-toe"):
     # cache control
     global stored_weights1
+    global stored_bias_1
     global stored_weights2
-    if stored_weights1 is None or stored_weights2 is None:
+    if stored_weights1 is None or stored_bias_1 is None or stored_weights2 is None:
         stored_weights1 = load_matrix_from_file(filename + "1")
+        stored_bias_1 = load_matrix_from_file(filename + "1b")
         stored_weights2 = load_matrix_from_file(filename + "2")
 
-    _, results = get_output(input, stored_weights1, stored_weights2)
+    _, results = get_output(input, stored_weights1, stored_bias_1, stored_weights2)
 
     return results
 
 
-def get_output(input, weights1, weights2):
+def get_output(input, weights1, bias1, weights2):
     if (
         input.shape != (inputs, 1)
         or weights1.shape != (hidden, inputs)
+        or bias1.shape != (hidden, 1)
         or weights2.shape != (outputs, hidden)
     ):
         raise ValueError("Wrong shapes")
 
-    hidden_layer = sigmoid(np.matmul(weights1, input))
+    hidden_layer = sigmoid(np.matmul(weights1, input) + bias1)
     result = sigmoid(np.matmul(weights2, hidden_layer))
     return (hidden_layer, result)
 
 
-def update_weights_step(weights1, weights2, input, expected_output):
+def update_weights_step(weights1, bias1, weights2, input, expected_output):
     if (
         input.shape != (inputs, 1)
         or expected_output.shape != (outputs, 1)
         or weights1.shape != (hidden, inputs)
+        or bias1.shape != (hidden, 1)
         or weights2.shape != (outputs, hidden)
     ):
         raise ValueError("Wrong shapes")
 
     # compute perceptron response
-    (hidden_layer, result) = get_output(input, weights1, weights2)
+    (hidden_layer, result) = get_output(input, weights1, bias1, weights2)
 
     # compute deltas
     delta_end = result * (1 - result) * (expected_output - result)  # sigmoid activated
@@ -70,9 +76,10 @@ def update_weights_step(weights1, weights2, input, expected_output):
 
     # update weights
     weights1 = weights1 + learn_rate * np.matmul(delta_hidden_layer, input.T)
+    bias1 = bias1 + learn_rate * delta_hidden_layer
     weights2 = weights2 + learn_rate * np.matmul(delta_end, hidden_layer.T)
 
-    return (weights1, weights2)
+    return (weights1, bias1, weights2)
 
 
 def sigmoid(x):
@@ -85,6 +92,7 @@ def train_perceptron(filename, iterations, input_output_pair, init_random=True):
         random_initial_matrices(filename)
 
     weights1 = load_matrix_from_file(filename + "1")
+    bias1 = load_matrix_from_file(filename + "1b")
     weights2 = load_matrix_from_file(filename + "2")
 
     # test shapes
@@ -92,6 +100,7 @@ def train_perceptron(filename, iterations, input_output_pair, init_random=True):
         input_output_pair()[0].shape != (inputs, 1)
         or input_output_pair()[1].shape != (outputs, 1)
         or weights1.shape != (hidden, inputs)
+        or bias1.shape != (hidden, 1)
         or weights2.shape != (outputs, hidden)
     ):
         raise ValueError("Wrong shapes")
@@ -103,8 +112,8 @@ def train_perceptron(filename, iterations, input_output_pair, init_random=True):
         input, expected_output = input_output_pair()
 
         # perform weight update for one step
-        (weights1, weights2) = update_weights_step(
-            weights1, weights2, input, expected_output
+        (weights1, bias1, weights2) = update_weights_step(
+            weights1, bias1, weights2, input, expected_output
         )
 
         percent = int(100.0 * i / iterations)
@@ -114,6 +123,7 @@ def train_perceptron(filename, iterations, input_output_pair, init_random=True):
 
     # store the matrices for later use
     store_matrix_to_file(weights1, filename + "1")
+    store_matrix_to_file(bias1, filename + "1b")
     store_matrix_to_file(weights2, filename + "2")
 
 
@@ -166,5 +176,5 @@ if __name__ == "__main__":
     # learn_logic_test()  # test the leraning process with a simple demo
 
     train_perceptron(
-        "tic-tac-toe", 40000, interface.random_input_output_pair, init_random=False
+        "tic-tac-toe", 4000000, interface.random_input_output_pair, init_random=False
     )
